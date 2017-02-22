c2e23c5f3345c6e51304925d6fde4358e1fe85e7
jhartman <jhartman@sfu.ca>
2017-02-21 22:00:05 -0500

    Update GraphicsClass
    Add methods for Initialize, ShutDown, and render

--- DirectX11_Tutorial_v1/graphicsclass.cpp
+++ DirectX11_Tutorial_v1/graphicsclass.cpp
@@ -1,12 +1,47 @@
+//////////////////////////////////////////////////////////////////////////
+// Filename: graphicsclass.cpp
+// Class: GraphicsClass
+// Description: Contains logic for the rendering of the window and scene. 
+// Author: Jeremy J. Hartmann
+//////////////////////////////////////////////////////////////////////////
 #include "graphicsclass.h"
 
 
 
 GraphicsClass::GraphicsClass()
 {
+    ;
 }
 
+GraphicsClass::GraphicsClass(const GraphicsClass& other)
+{
+    ;
+}
 
 GraphicsClass::~GraphicsClass()
 {
+    ;
+}
+
+bool GraphicsClass::Initialize(int screenWidth, int screenHeight, HWND in_hwnd)
+{
+    return true;
+}
+
+void GraphicsClass::Shutdown()
+{
+
+}
+
+bool GraphicsClass::Frame()
+{
+    return true;
+}
+
+//////////////////////////////////////////////////////////////////////////
+// PRIVATE
+//////////////////////////////////////////////////////////////////////////
+bool GraphicsClass::Render()
+{
+    return true;
 }
--- DirectX11_Tutorial_v1/graphicsclass.h
+++ DirectX11_Tutorial_v1/graphicsclass.h
@@ -1,8 +1,35 @@
-#pragma once
+//////////////////////////////////////////////////////////////////////////
+// Filename: graphicsclass.h
+// Class: GraphicsClass
+// Description: Contains logic for the rendering of the window and scene. 
+// Author: Jeremy J. Hartmann
+//////////////////////////////////////////////////////////////////////////
+
+#ifndef _GRAPHICSCLASS_H_
+#define _GRAPHICSCLASS_H_
+
+// INCLUDE
+#include <windows.h>
+
+// GLOBALS
+const bool FULL_SCREEN = false;
+const bool VSYNC_ENABLED = true;
+const float SCREEN_DEPTH = 1000.0f;
+const float SCREEN_NEAR = 0.1f;
+
 class GraphicsClass
 {
 public:
     GraphicsClass();
+    GraphicsClass(const GraphicsClass& other);
     ~GraphicsClass();
+
+    bool Initialize(int screenWidth, int screenHeight, HWND in_hwnd);
+    void Shutdown();
+    bool Frame();
+
+private:
+    bool Render();
 };
 
+#endif // !_GRAPHICSCLASS_H_
