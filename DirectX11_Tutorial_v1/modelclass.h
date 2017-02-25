9aea0f6e2c118827a5ef8199d554931f95ceb3cd
jhartman <jhartman@sfu.ca>
2017-02-24 19:19:28 -0500

    Add class definition to ModelClass

--- DirectX11_Tutorial_v1/modelclass.h
+++ DirectX11_Tutorial_v1/modelclass.h
@@ -1,8 +1,44 @@
-#pragma once
+//////////////////////////////////////////////////////////////////////////
+// Filename: modelclass.h
+// Function:
+//////////////////////////////////////////////////////////////////////////
+#ifndef _MODELCLASS_H_
+#define _MODELCLASS_H_
+
+// INCLUDE
+#include <d3d11.h>
+#include <DirectXMath.h>
+using namespace DirectX;
+
+
+// CLASS
 class ModelClass
 {
+private:
+    struct VertexType {
+        XMFLOAT3 position;
+        XMFLOAT4 color;
+    };
+
 public:
     ModelClass();
+    ModelClass(const ModelClass& other);
     ~ModelClass();
+
+    bool Initialize(ID3D11Device *device);
+    void Shutdown();
+    void Render(ID3D11DeviceContext *deviceContext);
+
+    int GetIndexCount();
+
+private:
+    bool InitializeBuffers(ID3D11Device *device);
+    void ShutdownBuffers();
+    void RenderBuffers(ID3D11DeviceContext *deviceContext);
+
+private:
+    ID3D11Buffer *m_vertexBuffer, *m_indexBuffer;
+    int m_vertexCount, m_indexCount;
 };
 
+#endif // !_MODELCLASS_H_
